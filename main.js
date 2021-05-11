638232a03758b649d9aba337969028ac575ad05f
techmahendran <mahendran361999@gmail.com>
2021-05-11 21:08:40 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const text = 'I am Mahendran Front-end Developer';
+
+let index = 0;
+
+function write(){
+    document.body.innerHTML = text.slice(0,index);
+    index++;
+
+    if(index > text){
+        index = 0;
+    }
+}
+
+setInterval(write,200);
